5c837bd6d74f51a8e014822cd8cac2643e978854
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 03:21:35 +0200

    refactor: add question text to question compound

--- src/Compounds/QuestionCompound.js
+++ src/Compounds/QuestionCompound.js
@@ -3,6 +3,7 @@ import QuestionCategory from '../Components/Question/QuestionCategory'
 import QuestionDifficulty from '../Components/Question/QuestionDifficulty'
 import QuestionNumber from '../Components/Question/QuestionNumber'
 import QuestionSectionWrapper from '../Components/Question/QuestionSectionWrapper'
+import QuestionText from '../Components/Question/QuestionText'
 
 export default QuestionCompound
 
@@ -13,6 +14,7 @@ function QuestionCompound ({ children }) {
         <QuestionNumber />
         <QuestionCategory />
         <QuestionDifficulty />
+        <QuestionText />
       </QuestionSectionWrapper>
       {children}
     </>
