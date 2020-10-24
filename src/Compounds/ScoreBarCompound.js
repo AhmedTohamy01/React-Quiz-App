760948b956fad410f0ad325ee6d7de70698b4a9e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 05:18:10 +0200

    refactor: add score bar wrapper to score bar compound

--- src/Compounds/ScoreBarCompound.js
+++ src/Compounds/ScoreBarCompound.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MaxScore from '../Components/ScoreBar/MaxScore'
 import ScoreBarData from '../Components/ScoreBar/ScoreBarData'
+import ScoreBarWrapper from '../Components/ScoreBar/ScoreBarWrapper'
 import ScoreSectionWrapper from '../Components/ScoreBar/ScoreSectionWrapper'
 import UserScore from '../Components/ScoreBar/UserScore'
 
@@ -14,6 +15,9 @@ function ScoreBarCompound ({ children }) {
           <UserScore>Score: 0 %</UserScore>
           <MaxScore>Max Score: 0 %</MaxScore>
         </ScoreBarData>
+        <ScoreBarWrapper>
+          
+        </ScoreBarWrapper>
       </ScoreSectionWrapper>
         {children}
     </>
