7167ca22702e0e6295c4cbf1fa5e5bcbaa8553b1
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 15:01:55 +0200

    feat: set progressbar as current question context consumer

--- src/Components/ProgressBar/ProgressBar.js
+++ src/Components/ProgressBar/ProgressBar.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProgressBar.css'
+import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
 
 export default ProgressBar
 
-function ProgressBar ({ children, ...restProps }) {
+function ProgressBar({ children, ...restProps }) {
+  const [currentQuestion] = useContext(CurrentQuestionContext)
+
   return (
     <div className='progress-bar' {...restProps}>
       {children}
