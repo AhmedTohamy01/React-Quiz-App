c002b1e27be89b13b55363d0ffc448d55da255fe
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 05:09:02 +0200

    feat: build user score componenet & add it in score bar compound

--- src/Compounds/ScoreBarCompound.js
+++ src/Compounds/ScoreBarCompound.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ScoreBarData from '../Components/ScoreBar/ScoreBarData'
 import ScoreSectionWrapper from '../Components/ScoreBar/ScoreSectionWrapper'
+import UserScore from '../Components/ScoreBar/UserScore'
 
 export default ScoreBarCompound
 
@@ -9,7 +10,7 @@ function ScoreBarCompound ({ children }) {
     <>
       <ScoreSectionWrapper>
         <ScoreBarData>
-          
+          <UserScore>Score: 0 %</UserScore>
         </ScoreBarData>
 
       </ScoreSectionWrapper>
