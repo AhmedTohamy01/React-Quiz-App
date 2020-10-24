f8bba5ff93646baa9ee87fb77c6e97e9b1fe88e6
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 16:26:33 +0200

    feat: set question difficulty to be a consumer of current question context

--- src/Components/Question/QuestionDifficulty.js
+++ src/Components/Question/QuestionDifficulty.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React, { useContext } from 'react'
 import "./Question.css"
+import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
 
 export default QuestionDifficulty
 
 function QuestionDifficulty({ children, ...restProps }) {
+  const [CurrentQuestion] = useContext(CurrentQuestionContext)
+
   return (
     <p className="question-difficulty" {...restProps}>
       ★★★
