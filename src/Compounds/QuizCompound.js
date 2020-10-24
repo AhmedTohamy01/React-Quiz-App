446090b825a606b48866e505cad816c554302f63
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 02:16:27 +0200

    refactor: add progress bar in quiz compound

--- src/Compounds/QuizCompound.js
+++ src/Compounds/QuizCompound.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import ProgressBar from '../Components/ProgressBar/ProgressBar'
 
 export default QuizCompound
 
 function QuizCompound ({ children }) {
-  return <>Quiz Compound</>
+  return <><ProgressBar /></>
 }
