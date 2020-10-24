445e9e04933ed017ded058d52d223863e41bff69
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 14:58:28 +0200

    feat: set quiz compound as the current question context provider

--- src/Compounds/QuizCompound.js
+++ src/Compounds/QuizCompound.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProgressBar from '../Components/ProgressBar/ProgressBar'
 import AnswerCompound from './AnswerCompound'
 import QuestionCompound from './QuestionCompound'
 import ScoreBarCompound from './ScoreBarCompound'
+import { CurrentQuestionContext } from '../Context/CurrentQuestionContext'
 
 export default QuizCompound
 
-function QuizCompound ({ children }) {
+function QuizCompound({ children }) {
+  const [currentQuestion, setCurrentQuestion] = useState(1)
+
   return (
     <>
-      <ProgressBar />
-      <QuestionCompound />
-      <AnswerCompound />
-      <ScoreBarCompound />
-      {children}
+      <CurrentQuestionContext.Provider value={[currentQuestion, setCurrentQuestion]}>
+        <ProgressBar />
+        <QuestionCompound />
+        <AnswerCompound />
+        <ScoreBarCompound />
+        {children}
+      </CurrentQuestionContext.Provider>
     </>
   )
 }
