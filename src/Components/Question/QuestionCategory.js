e2668e3632fee375cd3c1ea052b4efa6ac108b99
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 16:02:50 +0200

    feat: set question category to be a consumer of current question context

--- src/Components/Question/QuestionCategory.js
+++ src/Components/Question/QuestionCategory.js
@@ -1,9 +1,12 @@
 import React from "react"
 import "./Question.css"
+import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
 
 export default QuestionCategory
 
 function QuestionCategory({ children, ...restProps }) {
+  const [currentQuestion] = useContext(CurrentQuestionContext)
+
   return (
     <p className="question-category" {...restProps}>
       Movies: Board Games
