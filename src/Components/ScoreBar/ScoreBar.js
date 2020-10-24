efbe1c318577587f9a374ce0cf98988dcfa7e0ac
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 05:21:17 +0200

    feat: build score bar component & add its css code

---
+++ src/Components/ScoreBar/ScoreBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ScoreBar.css'
+
+export default ScoreBar
+
+function ScoreBar ({ children, ...restProps }) {
+  return <div className='score-bar' {...restProps}>{children}</div>
+}
