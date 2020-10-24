a31d4a6e15964d8131b2bd09f136079a6ac9f14b
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 15:53:00 +0200

    feat: set QuestionNumber to be a consumer for CurrentQuestionContext

--- src/Components/Question/QuestionNumber.js
+++ src/Components/Question/QuestionNumber.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Question.css"
+import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
 
 export default QuestionNumber
 
 function QuestionNumber({ children, ...restProps }) {
+  const [currentQuestion] = useContext(CurrentQuestionContext)
   return (
     <p className="question-number" {...restProps}>
       Question 1 of 20
