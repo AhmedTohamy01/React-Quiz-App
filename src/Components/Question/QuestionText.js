ca3bab8b3b01e7db57de0d20498f19df1c0a3a8e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 16:34:42 +0200

    feat: set question text to be a consumer of current question context

--- src/Components/Question/QuestionText.js
+++ src/Components/Question/QuestionText.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React, { useContext } from 'react'
 import "./Question.css"
+import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
 
 export default QuestionText
 
 function QuestionText({ children, ...restProps }) {
+  const [currentQuestion] = useContext(CurrentQuestionContext)
+
   return (
     <p className="question-text" {...restProps}>
       What was the name of the hero in the 80s animated video games ?
