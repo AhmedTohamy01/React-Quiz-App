68d3af5153882ccd8b06aa90467f3dc3f68a72ae
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 04:23:02 +0200

    fix: put next question button in right place

--- src/Compounds/AnswerCompound.js
+++ src/Compounds/AnswerCompound.js
@@ -3,6 +3,7 @@ import AnswerButton from "../Components/Answer/AnswerButton"
 import AnswerButtonsWrapper from "../Components/Answer/AnswerButtonsWrapper"
 import AnswerResult from "../Components/Answer/AnswerResult"
 import AnswerSectionWrapper from "../Components/Answer/AnswerSectionWrapper"
+import NextQuestionButton from "../Components/Answer/NextQuestionButton"
 import NextQuestionButtonWrapper from "../Components/Answer/NextQuestionButtonWrapper"
 
 export default AnswerCompound
@@ -20,7 +21,7 @@ function AnswerCompound({ children }) {
       </AnswerSectionWrapper>
       <AnswerResult>Correct Answer!</AnswerResult>
       <NextQuestionButtonWrapper>
-        <AnswerButton>Next Question</AnswerButton>
+        <NextQuestionButton>Next Question</NextQuestionButton>
       </NextQuestionButtonWrapper>
       {children}
     </>
