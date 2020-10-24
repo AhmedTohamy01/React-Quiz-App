cefdaf26f9a4289f6545b6d425f2b4d0e09a2693
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-24 02:36:58 +0200

    feat: build question compound

---
+++ src/Compounds/QuestionCompound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import QuestionSectionWrapper from '../Components/Question/QuestionSectionWrapper'
+
+export default QuestionCompound
+
+function QuestionCompound ({ children }) {
+  return (
+    <>
+      <QuestionSectionWrapper>
+        
+      </QuestionSectionWrapper>
+      {children}
+    </>
+  )
+}
